418ff44c2aa209f610a7ce38dacd3c7c73004376
“handsomeonion <1139858169@qq.com>
2018-11-21 14:40:01 +0800

    UpdateGoodsnum GoodsNum++ GoodsNum--

---
+++ src/main/java/com/Api/GoodsNumPlus.java
@@ -0,0 +1,49 @@
+package com.Api;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import com.MySql.MysqlUtil;
+import org.json.JSONObject;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class GoodsNumPlus extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        HttpSession session=request.getSession();
+        ArrayList<JSONObject> bookArr=(ArrayList<JSONObject>)session.getAttribute("bookArr");
+        PrintWriter out=response.getWriter();
+        String sbook_id=request.getParameter("book_id");
+        int book_id=Integer.parseInt(sbook_id);
+       // String sbook_id="100000158";
+       // int book_id=100000158;
+        for(int i=0;i<bookArr.size();i++)
+        {
+            JSONObject data=bookArr.get(i);
+            int id=data.getInt("book_id");
+            if(id==book_id)
+            {
+                JSONObject book=new JSONObject();
+                int book_num=data.getInt("book_num");
+                book_num++;
+                book.put("book_id",book_id);
+                book.put("book_num",book_num);
+                bookArr.set(i,book);
+                break;
+            }
+        }
+        session.setAttribute("bookArr",bookArr);
+    }
+}
--- src/main/java/com/Api/UpdateGoodsNum.java
+++ src/main/java/com/Api/UpdateGoodsNum.java
@@ -9,50 +9,50 @@ import java.io.IOException;
 import com.MySql.MysqlUtil;
 import org.json.JSONObject;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
-public class AddToShopCar extends HttpServlet {
+public class UpdateGoodsNum extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            response.setCharacterEncoding("UTF-8");
-            HttpSession session=request.getSession();//书的信息全部放在bookArr里
-           ArrayList<JSONObject> bookArr=(ArrayList<JSONObject>)session.getAttribute("bookArr");
-            if(bookArr==null)
-            {
-                bookArr=new ArrayList<JSONObject>();
-            }
-            String sbook_id=request.getParameter("book_id");
-            //String sbook_id="100000158";
-            //int book_id=100000158;
-           int book_id=Integer.parseInt(sbook_id);
-            int flag=1;
+        response.setCharacterEncoding("UTF-8");
+        HttpSession session=request.getSession();
+        ArrayList<JSONObject> bookArr=(ArrayList<JSONObject>)session.getAttribute("bookArr");
+        PrintWriter out=response.getWriter();
+        String sbook_id=request.getParameter("book_id");
+       int book_id=Integer.parseInt(sbook_id);
+        //String sbook_id="100000158";
+        //int book_id=100000158;
+        String newNum=request.getParameter("book_num");
+        int num=Integer.parseInt(newNum);
+        //String newNum="5";
+        //int num=5;
+        if(num<0)
+        {
+            out.println(ResJson.generateResJson(-1, "Error", "商品数必须为正整数"));
+        }
+        else
+        {
             for(int i=0;i<bookArr.size();i++)
             {
                 JSONObject data=bookArr.get(i);
                 int id=data.getInt("book_id");
                 if(id==book_id)
                 {
-                    flag=0;
-                    int book_num=data.getInt("book_num");
-                    book_num++;
                     JSONObject book=new JSONObject();
                     book.put("book_id",id);
-                    book.put("book_num",book_num);
+                    book.put("book_num",num);
                     bookArr.set(i,book);
                     break;
                 }
-            }
-            if(flag==1)
-            {
-                JSONObject book=new JSONObject();
-                book.put("book_id",book_id);
-                book.put("book_num",1);
-                bookArr.add(book);
             }
             session.setAttribute("bookArr",bookArr);
         }
 
     }
+}
